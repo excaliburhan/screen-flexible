edecac6d964aedcec5cd74001a3b5441171d4245
xiaoping <edwardhjp@gmail.com>
2017-07-24 11:55:32 +0800

    Change: change getScale to bodyResize && add domResize

--- src/index.js
+++ src/index.js
@@ -2,34 +2,42 @@
  * @author xiaoping
  * @email edwardhjp@gmail.com
  * @create date 2017-06-01 04:33:34
- * @modify date 2017-06-01 04:33:34
+ * @modify date 2017-07-24 11:17:12
  * @desc [scale methods]
 */
 
-function getScale(width = 1920, height = 1080) {
-  let docEl = document.documentElement
-  let bodyEl = document.body
-  // set docEl
+function bodyResize (width = 1920, height = 1080, type = 'padding') {
+  const docEl = document.documentElement
+  const bodyEl = document.body
+  // 设置docEl初始样式
   docEl.style.width = '100%'
   docEl.style.height = '100%'
-  let scale, top, left
-  let offsetWidth = docEl.offsetWidth
-  let offsetHeight = docEl.offsetHeight
-  let scaleX = offsetWidth / width
-  let scaleY = offsetHeight / height
-  if (scaleX < scaleY) {
+  const offsetWidth = docEl.offsetWidth
+  const offsetHeight = docEl.offsetHeight
+  const scaleX = offsetWidth / width
+  const scaleY = offsetHeight / height
+  let top = 0
+  let left = 0
+  let scale
+  if (type === 'padding') {
+    if (scaleX < scaleY) {
+      scale = scaleX
+      top = (offsetHeight - height * scale) / 2
+    } else if (scaleX > scaleY) {
+      scale = scaleY
+      left = (offsetWidth - width * scale) / 2
+    } else {
+      // 同步放大/缩小
+      scale = scaleX
+    }
+  } else if (type === 'width') {
     scale = scaleX
-    top = (offsetHeight - height * scale) / 2
-    left = 0
-  } else if (scaleX > scaleY) {
+  } else if (type === 'height') {
     scale = scaleY
-    top = 0
-    left = (offsetWidth - width * scale) / 2
-  } else {
-    scale = scaleX // 同步放大/缩小
-    top = 0
-    left = 0
+  } else if (type === 'full') {
+    // do nothing
   }
+
   scale = scale.toFixed(6)
   top = top.toFixed(4)
   left = left.toFixed(4)
@@ -38,13 +46,39 @@ function getScale(width = 1920, height = 1080) {
   bodyEl.style.transformOrigin = 'left top 0px'
   bodyEl.style.webkitTransformOrigin = 'left top 0px'
   bodyEl.style.MozTransformOrigin = 'left top 0px'
-  bodyEl.style.transform = `scale(${scale})`
-  bodyEl.style.webkitTransform = `scale(${scale})`
-  bodyEl.style.MozTransform = `scale(${scale})`
+  // 全部铺满
+  if (type === 'full') {
+    bodyEl.style.transform = `scale(${scaleX}, ${scaleY})`
+    bodyEl.style.webkitTransform = `scale(${scaleX}, ${scaleY})`
+    bodyEl.style.MozTransform = `scale(${scaleX}, ${scaleY})`
+  } else {
+    bodyEl.style.transform = `scale(${scale})`
+    bodyEl.style.webkitTransform = `scale(${scale})`
+    bodyEl.style.MozTransform = `scale(${scale})`
+  }
   bodyEl.style.marginLeft = `${left}px`
   bodyEl.style.marginTop = `${top}px`
 }
 
+function domResize (dom, parentDom, width = '1920', height = '1080') {
+  const offsetWidth = parentDom.offsetWidth
+  const offsetHeight = parentDom.offsetHeight
+  const scaleX = offsetWidth / width
+  const scaleY = offsetHeight / height
+  let scale = scaleX < scaleY ? scaleX : scaleY
+
+  scale = scale.toFixed(6)
+  dom.style.width = width + 'px'
+  dom.style.height = height + 'px'
+  dom.style.transformOrigin = 'left top 0px'
+  dom.style.webkitTransformOrigin = 'left top 0px'
+  dom.style.MozTransformOrigin = 'left top 0px'
+  dom.style.transform = `scale(${scale})`
+  dom.style.webkitTransform = `scale(${scale})`
+  dom.style.MozTransform = `scale(${scale})`
+}
+
 module.exports = {
-  getScale,
+  bodyResize,
+  domResize
 }
